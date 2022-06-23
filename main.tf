e6ebfa9b02865002fc66c8e7652e4a93bbd0fa74
litscherk1 <kyle.g.litscher@lmco.com>
2022-06-23 21:24:44 +0000

    Added s3 bucket module

--- main.tf
+++ main.tf
@@ -128,7 +128,10 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    Department = "devops"
+    Department = [
+      "devops",
+      "Billable"
+    ]
   }
 }
 
